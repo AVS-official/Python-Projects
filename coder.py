2029ebe3211c9459c87d9b417d69a21ad277b7f1
Aditya Vikram Srivastava <forhumanity7@outlook.com>
2025-07-20 18:08:34 +0530

    coder.py

--- coder.py
+++ coder.py
@@ -1,29 +1,28 @@
-s = input("Enter a Sentence: ")
+s = input("Enter a Message: ")
 words = s.split(' ')
 
-a = "qwe"
-b = "zxc"
+coding = int(input("Enter 1 for coding and 0 for decoding: "))
 
-coding = input("Type Code(1) for Encrypting\nType Decode(0) for Decrypting\n\nType: ")
-
-if(coding):
+if(coding == 1):
     nwords = []
     for word in words:
         if(len(word) >= 3):
-            sNew = a + (word[1:] + word[0])[::-1] + b   
-            nwords.append(sNew)
+            r1 = "aef"
+            r2 = "pse"
+            s1 = r1 + word[1:] + word[0] + r2
+            nwords.append(s1)
         else:
-            snew = a + word[::-1] + b
-            nwords.append(snew)
-    print("".join(nwords))
-else:
+            nwords.append(word[::-1])
+    print(' '.join(nwords))
+if(coding == 0):
     nwords = []
     for word in words:
         if(len(word) >= 3):
-            sNew = word[::-1][3:-3]
-            sNew = sNew[-1] + sNew[:-1]
-            nwords.append(sNew)
+            r1 = "aef"
+            r2 = "pse"
+            s1 = word[3:-3]
+            s3 = s1[-1] + s1[:-1]
+            nwords.append(s3)
         else:
-            snew = word[::-1][3:-3]
-            nwords.append(snew)
-    print("".join(nwords))
+            nwords.append(word[::-1])
+    print(' '.join(nwords))
